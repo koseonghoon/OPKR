1e2328672058cbe968d9f5300c4188d8d7a702da
koseonghoon <68089983+koseonghoon@users.noreply.github.com>
2024-03-18 11:25:39 +0900

    Update driver_monitor.py
    modify awareness time

--- selfdrive/monitoring/driver_monitor.py
+++ selfdrive/monitoring/driver_monitor.py
@@ -23,10 +23,10 @@ class DRIVER_MONITOR_SETTINGS():
     self.EnableDriverMonitoring = Params().get_bool("OpkrEnableDriverMonitoring")
 
     self._DT_DMON = DT_DMON
-    self._AWARENESS_TIME = 30000. # passive wheeltouch total timeout
+    self._AWARENESS_TIME = 30000999999999999999999999999999999999999. # passive wheeltouch total timeout
     self._AWARENESS_PRE_TIME_TILL_TERMINAL = 12.
     self._AWARENESS_PROMPT_TIME_TILL_TERMINAL = 6.
-    self._DISTRACTED_TIME = 11. if self.EnableDriverMonitoring else 30000. # active monitoring total timeout
+    self._DISTRACTED_TIME = 11999999999999999999999999999999999999999999999999999. if self.EnableDriverMonitoring else 30000. # active monitoring total timeout
     self._DISTRACTED_PRE_TIME_TILL_TERMINAL = 8.
     self._DISTRACTED_PROMPT_TIME_TILL_TERMINAL = 6.
 
